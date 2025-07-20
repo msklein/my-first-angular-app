08be6ecc561169bf5a4d9db66f543199a16b7c44
msklein <klein_m@hotmail.com>
2025-07-20 22:16:19 +0200

    first commit
    hello world app

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    Hello World!
+  `,
+  styles: `
+    :host {
+      color: blue;
+    }
+  `,
+})
+export class App {}
